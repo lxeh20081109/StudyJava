907acf24371712faf591fbf39c0c41f3b5da128f
LX <69516995+lxeh20081109@users.noreply.github.com>
2020-08-15 15:12:10 +0900

    Add files via upload

---
+++ Java_StringLink.java
@@ -0,0 +1,13 @@
+package Study;
+
+public class Java_StringLink {
+		public static void main(String[] args){
+			int num = 10;
+			String text = "こんにちわ";
+			
+			System.out.println(num + text);
+		}
+}
+/*
+10こんにちわ
+*/
