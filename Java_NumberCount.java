db937345f817d20cfd2456db8b9c866a0b6c9b9e
LX <69516995+lxeh20081109@users.noreply.github.com>
2020-08-15 15:16:02 +0900

    Add files via upload

---
+++ Java_NumberCount.java
@@ -0,0 +1,18 @@
+package Study;
+
+public class Java_NumberCount {
+		public static void main(String[] args){
+			int a = 8;
+			int b = 26;
+			
+			int x = (4*a-b)/2;
+			int y = b/2-a;
+			
+			System.out.println("x=" + x);
+			System.out.println("y=" + y);
+		}
+}
+/*
+x=3
+y=5
+*/
